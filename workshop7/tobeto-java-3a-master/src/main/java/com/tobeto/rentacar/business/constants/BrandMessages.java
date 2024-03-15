44260ad81de811644f321f4f422676614dd9fe97
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-03-15 21:15:43 +0300

    Delete workshop7/tobeto-java-3a-master/src/main/java/com/tobeto/rentacar/business/constants/BrandMessages.java

--- workshop7/tobeto-java-3a-master/src/main/java/com/tobeto/rentacar/business/constants/BrandMessages.java
+++
@@ -1,7 +0,0 @@
-package com.tobeto.rentacar.business.constants;
-
-public class BrandMessages {
-
-	public static final String BrandAdded = "Brand Added Successfully";
-	public static final String BrandListed = "Brand Listed Successfully";
-}
