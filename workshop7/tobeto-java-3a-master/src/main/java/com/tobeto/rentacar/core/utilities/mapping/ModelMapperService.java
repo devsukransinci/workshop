2476abadf14d6add481146350eca80ba106a31b7
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-03-15 21:17:05 +0300

    Delete workshop7/tobeto-java-3a-master/src/main/java/com/tobeto/rentacar/core/utilities/mapping/ModelMapperService.java

--- workshop7/tobeto-java-3a-master/src/main/java/com/tobeto/rentacar/core/utilities/mapping/ModelMapperService.java
+++
@@ -1,9 +0,0 @@
-package com.tobeto.rentacar.core.utilities.mapping;
-
-import org.modelmapper.ModelMapper;
-
-public interface ModelMapperService {
-
-	ModelMapper forRequest();
-	ModelMapper forResponse();
-}
